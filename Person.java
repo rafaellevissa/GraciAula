75c056404d22b6c359aee15cfa1e24f28c73bf02
Rafael Levi <rafaellevissa@gmail.com>
2025-01-28 18:17:03 +0000

    adjusts

--- Person.java
+++ Person.java
@@ -1,32 +1,20 @@
-package GraciAula;
 // Definition of the Person class
 public class Person {
-    // Private attributes (encapsulation)
     private String name;
     private int age;
-
-    // Constructor
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
-
-    // Getter method for retrieving the name
     public String getName() {
         return name;
     }
-
-    // Setter method for modifying the name
     public void setName(String name) {
         this.name = name;
     }
-
-    // Getter method for retrieving the age
     public int getAge() {
         return age;
     }
-
-    // Setter method for modifying the age
     public void setAge(int age) {
         if (age > 0) {
             this.age = age;
@@ -34,14 +22,10 @@ public class Person {
             System.out.println("Invalid age!");
         }
     }
-
-    // Method to display person information
     public void displayInformation() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age + " years old");
     }
-
-    // Main method to test the class
     public static void main(String[] args) {
         // Creating an object of the Person class
         Person person1 = new Person("David", 30);
